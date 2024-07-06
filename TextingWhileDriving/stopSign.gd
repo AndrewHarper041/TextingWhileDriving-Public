a274914be0a4ad4a3e33da2061928c4c38ed7629
SA <srabate@gmail.com>
2024-07-05 22:54:09 -0400

    traffic violation detection - initial

--- TextingWhileDriving/stopSign.gd
+++ TextingWhileDriving/stopSign.gd
@@ -5,6 +5,8 @@ var stop_sign_unit_vector: Vector3 = Vector3(0,0,1)
 var contained_body
 const ENFORCEMENT_THRESHOLD = -5.0
 var min_stop_sign_projected_speed: float
+#@onready var traffic_violation_label = $car/Hud/traffic_violation
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -27,10 +29,10 @@ func _on_body_entered(body):
 	min_stop_sign_projected_speed = get_body_projected_speed(contained_body)
 	
 func _on_body_exited(body):
-	contained_body = null
-	
 	if min_stop_sign_projected_speed < ENFORCEMENT_THRESHOLD:
 		print("=========== TRAFFIC VIOLATION !!! ===========")
 		print("Body min speed: " + str(min_stop_sign_projected_speed))
-	
+		body.get_node("Hud/traffic_violation").text = "TRAFFIC VIOLATION!"
+
+	contained_body = null
 	min_stop_sign_projected_speed = 0.0
