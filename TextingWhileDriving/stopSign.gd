5ced600b6e7b3bcc3b0b11a1816aa049ef6c1114
Luke Wilson <luqwil@gmail.com>
2024-07-06 19:02:52 -0400

    Fix collision layers and programatically set stopsign vector. Checked into main because I'm a bad programmer.

--- TextingWhileDriving/stopSign.gd
+++ TextingWhileDriving/stopSign.gd
@@ -6,12 +6,12 @@ var contained_body
 const ENFORCEMENT_THRESHOLD = -5.0
 var min_stop_sign_projected_speed: float
 #@onready var traffic_violation_label = $car/Hud/traffic_violation
+@onready var collision_shape_3d = $CollisionShape3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# TODO - obtain actual normal vector that accounts for rotation etc.
-	stop_sign_unit_vector = Vector3(0,0,1)
-	pass
+	stop_sign_unit_vector = collision_shape_3d.global_transform.basis.z
 
 # Calculate body speed perpendicular to stop sign collision box
 func get_body_projected_speed(body):
@@ -22,7 +22,6 @@ func _process(delta):
 	if contained_body:
 		var stop_sign_projected_speed: float = get_body_projected_speed(contained_body)
 		print("Projected Speed: " + str(stop_sign_projected_speed))
-		
 		min_stop_sign_projected_speed = min(min_stop_sign_projected_speed, stop_sign_projected_speed)
 
 func _on_body_entered(body):
